e31c5e79687106485b8dfb7449f05ea4dc2dc025
Joseph-K-B <303josephb@gmail.com>
2021-09-06 13:25:10 -0700

    basic structure and styling for first three divs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Main extends Component {
+  state = {  }
+  render() { 
+    return ( 
+    <>
+      <section className='head-section'>
+        <div className='top-div'>
+          <h1 className='name'>Joseph Brown</h1>
+          <h3 className='name-intro'>Software Engineer, Musician, Collaborator</h3>
+        </div>
+      </section>
+      <section className="main-section">
+        <div className='left-middle'>
+          <h2>Languages</h2>
+          <ul>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
+        </div>
+        {/* <div className='right-middle'>
+          <h2>Design</h2>
+            <p className='right-middle-p'>blah</p>
+          <h2>Develop</h2>
+            <p className='right-middle-p'>blah</p>
+          <h2>Full-Stack</h2>
+            <p className='right-middle-p'>blah</p>
+          <h2>Collaborate</h2>
+            <p className='right-middle-p'>blah</p>
+        </div> */}
+        <div className='right-middle'>
+          <h2>Design</h2>
+          <h2>Develop</h2>
+          <h2>Full-Stack</h2>
+          <h2>Collaborate</h2>
+            <p className='design-p'>blah</p>
+            <p className='develop-p'>blah</p>
+            <p className='full-stack-p'>blah</p>
+            <p className='collab-p'>blah</p>
+        </div>
+      </section>
+   </> 
+    );
+  }
 }
-
-export default App;
+ 
+export default Main;
