a7581c4d3ab2b4551352c5a53eaaae6752acc434
Joseph-K-B <303josephb@gmail.com>
2021-09-06 14:24:02 -0700

    main section left/right formatted

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class Main extends Component {
           <h3 className='name-intro'>Software Engineer, Musician, Collaborator</h3>
         </div>
       </section>
-      <section className="main-section">
+      <section className="top-section">
         <div className='left-middle'>
           <h2>Languages</h2>
           <ul>
@@ -21,16 +21,6 @@ class Main extends Component {
             <li>CSS</li>
           </ul>
         </div>
-        {/* <div className='right-middle'>
-          <h2>Design</h2>
-            <p className='right-middle-p'>blah</p>
-          <h2>Develop</h2>
-            <p className='right-middle-p'>blah</p>
-          <h2>Full-Stack</h2>
-            <p className='right-middle-p'>blah</p>
-          <h2>Collaborate</h2>
-            <p className='right-middle-p'>blah</p>
-        </div> */}
         <div className='right-middle'>
           <h2>Design</h2>
           <h2>Develop</h2>
@@ -40,6 +30,36 @@ class Main extends Component {
             <p className='develop-p'>blah</p>
             <p className='full-stack-p'>blah</p>
             <p className='collab-p'>blah</p>
+        </div>
+        <div className='left-bottom'>
+          <h2>Contact</h2>
+          <ul>
+            <li><a>LinkedIn</a></li>
+            <li><a>Email</a></li>
+            <li><a>Github</a></li>
+          </ul>
+        </div>      
+        <div className='right-bottom'>
+          <div>
+          <h2>Project 1</h2>
+            <p>project 1 description</p>
+            <a>project 1 link</a>
+            </div>
+            <div>
+          <h2>Project 2</h2>
+            <p>project 2 description</p>
+            <a>project 2 link</a>
+            </div>
+            <div>
+          <h2>Project 3</h2>
+            <p>project 3 description</p>
+            <a>project 3 link</a>
+            </div>
+            <div>
+          <h2>Project 4</h2>
+            <p>project 4 description</p>
+            <a>project 4 link</a>
+            </div>
         </div>
       </section>
    </> 
